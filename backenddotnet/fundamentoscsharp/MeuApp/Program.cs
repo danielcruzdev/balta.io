53e28c23dba58a49aa8a3dd40b7be2fad811e007
Daniel Cruz <d.afc98@gmail.com>
2021-06-13 19:57:50 -0300

    utilizando método

--- backenddotnet/fundamentoscsharp/MeuApp/Program.cs
+++ backenddotnet/fundamentoscsharp/MeuApp/Program.cs
@@ -6,31 +6,45 @@ namespace MeuApp
     {
         static void Main(string[] args)
         {
-            var texto = "Testando";
-            Console.WriteLine(texto);
+            #region Parse/Convert/Operadores
+            // var texto = "Testando";
+            // Console.WriteLine(texto);
 
-            //Conversão Implicita de Dados
-            float valor = 25.8f;
-            int outro = 25;
+            // //Conversão Implicita de Dados
+            // float valor = 25.8f;
+            // int outro = 25;
 
-            valor = outro;
-            Console.WriteLine(valor);
+            // valor = outro;
+            // Console.WriteLine(valor);
 
-            //Conversão Explicita de Dados
-            int inteiro = 100;
-            uint inteiroSemSinal = (uint)inteiro;
-            Console.WriteLine(inteiroSemSinal);
+            // //Conversão Explicita de Dados
+            // int inteiro = 100;
+            // uint inteiroSemSinal = (uint)inteiro;
+            // Console.WriteLine(inteiroSemSinal);
 
-            //Parse - Sempre espera uma string
-            int numeroInteiro = int.Parse("100");
+            // //Parse - Sempre espera uma string
+            // int numeroInteiro = int.Parse("100");
 
-            //Convert - Permite que a gente converta um tipo para outro tipo
-            int numeroInteiro2 = Convert.ToInt32(20.8);
+            // //Convert - Permite que a gente converta um tipo para outro tipo
+            // int numeroInteiro2 = Convert.ToInt32(20.8);
 
-            //Operadores de atribuição
-            int x = 2;
-            x = 2 + 2;
-            Console.WriteLine(x);
+            // //Operadores de atribuição
+            // int x = 2;
+            // x = 2 + 2;
+            // Console.WriteLine(x);
+            #endregion
+
+            #region Funções e Metodos
+            MeuMetodo("Utilizando Método!");
+            #endregion            
         }
+
+
+
+        static void MeuMetodo(string parametro)
+        {
+            Console.WriteLine(parametro);
+        }
+        
     }
 }
