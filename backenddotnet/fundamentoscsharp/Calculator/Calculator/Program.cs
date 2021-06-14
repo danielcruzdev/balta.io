de2046f0d2ad9e5732a15e997ed305403fab5917
Daniel Cruz <d.afc98@gmail.com>
2021-06-13 22:10:56 -0300

    projeto calculadora - soma

---
+++ backenddotnet/fundamentoscsharp/Calculator/Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.Write("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 + v2;
+            Console.WriteLine("--------------------------");
+            Console.WriteLine($"Soma: {resultado}");
+        }
+    }
+}
