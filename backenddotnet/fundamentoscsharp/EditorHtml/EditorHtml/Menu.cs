e3868907eb878aa97dfd6fa0574d8bf298a4625f
Daniel Cruz <d.afc98@gmail.com>
2021-06-22 23:12:01 -0300

    utilizando opções no menu

--- backenddotnet/fundamentoscsharp/EditorHtml/EditorHtml/Menu.cs
+++ backenddotnet/fundamentoscsharp/EditorHtml/EditorHtml/Menu.cs
@@ -13,7 +13,40 @@ namespace EditorHtml
             DrawScreen(30, 10);
             WriteOptions();
 
-            var option = short.Parse(Console.ReadLine());
+            short option;
+            
+            var hasOption = short.TryParse(Console.ReadLine(), out option);
+            if(hasOption){
+                HandleMenuOption(option);
+            }
+            else {
+                Console.WriteLine("Erro ao escolher opção! Tente novamente.");
+                Show();
+            }
+        }
+
+        public static void HandleMenuOption(short option)
+        {
+            switch(option) 
+            {
+                case 1: {
+                    Console.WriteLine("Editor"); 
+                    break;
+                }
+                case 2: {
+                    Console.WriteLine("View");
+                    break;
+                }
+                case 0: {
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+                }
+                default: {
+                    Show();
+                    break;
+                }
+            }
         }
 
         public static void WriteOptions()
