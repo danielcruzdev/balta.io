9d97ef0e7420e4dbc5986ae8476de65dff0c92ae
Daniel Cruz <d.afc98@gmail.com>
2021-06-23 23:02:39 -0300

    obtendo valores de datetime

--- backenddotnet/fundamentoscsharp/Datas/Datas/Program.cs
+++ backenddotnet/fundamentoscsharp/Datas/Datas/Program.cs
@@ -7,9 +7,15 @@ namespace Datas
         static void Main(string[] args)
         {
             Console.Clear();
-            var data = new DateTime();
-            var dataHoje = DateTime.Now;
-            Console.WriteLine(dataHoje);
+            var data = new DateTime(2020, 10, 12, 8, 23, 14);
+            //var dateNow = DateTime.Now;
+            Console.WriteLine(data);
+            Console.WriteLine(data.Year);
+            Console.WriteLine(data.Month);
+            Console.WriteLine(data.Day);
+            Console.WriteLine(data.Hour);
+            Console.WriteLine(data.Minute);
+            Console.WriteLine(data.Second);
         }
     }
 }
