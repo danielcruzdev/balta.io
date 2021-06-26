763ef541ba44df4605bd409589c61bb358bf0d10
Daniel Cruz <d.afc98@gmail.com>
2021-06-26 00:09:48 -0300

    aprendendo a trabalhar com moedas

---
+++ backenddotnet/fundamentoscsharp/Moedas/Moedas/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Moedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            decimal valor = 10.25m;
+            Console.WriteLine(valor);
+
+
+        }
+    }
+}
