84a95b09a542ad84b568dcf12e2ca978305c5c8f
Daniel Cruz <d.afc98@gmail.com>
2021-06-27 21:11:07 -0300

    criando e inicializando array

--- backenddotnet/fundamentoscsharp/Arrays/Arrays/Program.cs
+++ backenddotnet/fundamentoscsharp/Arrays/Arrays/Program.cs
@@ -6,7 +6,16 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+
+            var meuArray = new int[5] { 1, 2, 3, 4, 5 };
+            meuArray[0] = 12;
+
+            Console.WriteLine(meuArray[0]);
+            Console.WriteLine(meuArray[1]);
+            Console.WriteLine(meuArray[2]);
+            Console.WriteLine(meuArray[3]);
+            Console.WriteLine(meuArray[4]);
         }
     }
 }
