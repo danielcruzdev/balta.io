d53faf9bf4bda0d1dfa9754c7d76c60fa8ccfa9a
Daniel Cruz <d.afc98@gmail.com>
2021-06-28 20:43:09 -0300

    utilizando try catch

--- backenddotnet/fundamentoscsharp/Exceptions/Exceptions/Program.cs
+++ backenddotnet/fundamentoscsharp/Exceptions/Exceptions/Program.cs
@@ -6,7 +6,21 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
-            
+            var array = new int[3];
+
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Inner Exception: {ex.InnerException}");
+                Console.WriteLine($"Message: {ex.Message}");
+                Console.WriteLine($"Oops! Algo errado aconteceu...");
+            }
         }
     }
 }
