1f15df3130aa025432196075d703f1b5b7082329
Daniel Cruz <d.afc98@gmail.com>
2021-06-27 21:27:15 -0300

    alterando valores de arrays

--- backenddotnet/fundamentoscsharp/Arrays/Arrays/Program.cs
+++ backenddotnet/fundamentoscsharp/Arrays/Arrays/Program.cs
@@ -16,6 +16,23 @@ namespace Arrays
             {
                 Console.WriteLine(numero);
             }
+
+            var arr = new int[4];
+            var arr2 = arr;
+
+            arr[0] = 12;
+
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr2[0]);
+
+            var primeiroArray = new int[4];
+            var segundoArray = new int[4];
+
+            segundoArray[0] = primeiroArray[0];
+
+            primeiroArray[0] = 23;
+
+            Console.WriteLine(segundoArray[0]);
         }
     }
 }
