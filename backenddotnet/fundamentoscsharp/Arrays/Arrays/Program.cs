ae000dd49b84c01ed4cf4d74939e3682d6badfa1
Daniel Cruz <d.afc98@gmail.com>
2021-06-27 21:16:01 -0300

    percorendo array

--- backenddotnet/fundamentoscsharp/Arrays/Arrays/Program.cs
+++ backenddotnet/fundamentoscsharp/Arrays/Arrays/Program.cs
@@ -8,14 +8,14 @@ namespace Arrays
         {
             Console.Clear();
 
-            var meuArray = new int[5] { 1, 2, 3, 4, 5 };
-            meuArray[0] = 12;
+            var numeros = new int[5] { 1, 2, 3, 4, 5 };
+            numeros[0] = 12;
 
-            Console.WriteLine(meuArray[0]);
-            Console.WriteLine(meuArray[1]);
-            Console.WriteLine(meuArray[2]);
-            Console.WriteLine(meuArray[3]);
-            Console.WriteLine(meuArray[4]);
+
+            foreach (var numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
         }
     }
 }
