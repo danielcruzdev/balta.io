9a5490e20360dbb0f4be9f3d16a42c5ab8220bf6
Daniel Cruz <d.afc98@gmail.com>
2021-06-28 21:19:13 -0300

    criando exceção

--- backenddotnet/fundamentoscsharp/Exceptions/Exceptions/Program.cs
+++ backenddotnet/fundamentoscsharp/Exceptions/Exceptions/Program.cs
@@ -10,16 +10,38 @@ namespace Exceptions
 
             try
             {
-                for (int i = 0; i < 10; i++)
-                {
-                    Console.WriteLine(array[i]);
-                }
+                //for (int i = 0; i < 10; i++)
+                //{
+                //    Console.WriteLine(array[i]);
+                //}
+
+                Cadastrar(null);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine($"Inner Exception: {ex.InnerException}");
+                Console.WriteLine($"Message: {ex.Message}");
+                Console.WriteLine($"Oops! Não encontrei o index no array...");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Inner Exception: {ex.InnerException}");
+                Console.WriteLine($"Message: {ex.Message}");
+                Console.WriteLine($"Oops! Falha ao cadastrar texto");
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Inner Exception: {ex.InnerException}");
                 Console.WriteLine($"Message: {ex.Message}");
                 Console.WriteLine($"Oops! Algo errado aconteceu...");
+            }
+        }
+
+        static void Cadastrar(string texto)
+        {
+            if (string.IsNullOrEmpty(texto))
+            {
+                throw new ArgumentNullException("O texto não pode ser nulo");
             }
         }
     }
