65f6ad7efd334bbf26a9397948d25decef770cea
Daniel Cruz <d.afc98@gmail.com>
2021-07-01 20:57:01 -0300

    sobrecarga de método

--- backenddotnet/fundamentos-orientacao-objetos/Payments/Payments/Program.cs
+++ backenddotnet/fundamentos-orientacao-objetos/Payments/Payments/Program.cs
@@ -1,40 +1,19 @@
-﻿using System;
-
-namespace Payments
+﻿namespace Payments
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var pagamento = new Pagamento();
-            pagamento.DataPagamento2 = DateTime.Now;
+
         }
     }
 
     public class Pagamento
     {
-        //Propriedades 
-        public DateTime Vencimento { get; set; }
-
-        private DateTime DataPagamento;
-
-        public DateTime DataPagamento2
-        {
-            get
-            {
-                Console.WriteLine("Lendo Valor");
-                return DataPagamento;
-            }
-            set
-            {
-                Console.WriteLine("Atribuindo Valor");
-                DataPagamento = value;
-            }
-        }
-
-
         //Métodos
-        public void Pagar() { }
+        public void Pagar(string numero) { }
+        public void Pagar(string numero, int numero2) { }
+        public void Pagar(string numero, int numero2, int numero3) { }
     }
 
     public class Endereco
