3bc312ea30899134df3f61c4bd12bbcb048a4c14
Daniel Cruz <d.afc98@gmail.com>
2021-07-03 00:39:31 -0300

    generic types parte 1

--- backenddotnet/fundamentos-orientacao-objetos/Payments/Payments/Program.cs
+++ backenddotnet/fundamentos-orientacao-objetos/Payments/Payments/Program.cs
@@ -1,56 +1,26 @@
-﻿using System;
-
-namespace Payments
+﻿namespace Payments
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var room = new Room(3);
-            room.RoomSoldOutEvent += OnRoomSoldOut;
-            room.ReserveSeat();
-            room.ReserveSeat();
-            room.ReserveSeat();
-            room.ReserveSeat();
-        }
-
-        static void OnRoomSoldOut(object sender, EventArgs eventArgs)
-        {
-            Console.WriteLine("Sala Lotada");
+            var person = new Person();
+            var context = new DataContext<Person>();
+            context.Save(person);
         }
     }
 
-    public class Room
+    public class DataContext<T>
     {
-        public Room(int seats)
+        public void Save(T entity)
         {
-            Seats = seats;
-            SeatsInUser = 0;
-        }
 
-        private int SeatsInUser = 0;
-        public int Seats { get; set; }
-
-        public void ReserveSeat()
-        {
-            SeatsInUser++;
-            if (SeatsInUser >= Seats)
-            {
-                //Evento fechado!
-                OnRoomSoldOut(EventArgs.Empty);
-            }
-            else
-            {
-                Console.WriteLine("Acento Reservado!");
-            }
-        }
-
-        public event EventHandler RoomSoldOutEvent;
-
-        protected virtual void OnRoomSoldOut(EventArgs eventArgs)
-        {
-            EventHandler handler = RoomSoldOutEvent;
-            handler?.Invoke(this, eventArgs);
         }
     }
+
+    public class Person { }
+
+    public class Payments { }
+
+    public class Subscription { }
 }
