00f6240cae9e52a0d03af5b326f81642e6567e4c
Daniel Cruz <d.afc98@gmail.com>
2021-07-03 22:31:06 -0300

    interagindo com listas

--- backenddotnet/fundamentos-orientacao-objetos/Projeto1/Projeto1/Program.cs
+++ backenddotnet/fundamentos-orientacao-objetos/Projeto1/Projeto1/Program.cs
@@ -2,6 +2,7 @@
 using Projeto1.ContentContext.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Projeto1
 {
@@ -20,6 +21,33 @@ namespace Projeto1
                 Console.WriteLine(article.Title);
                 Console.WriteLine(article.Url);
                 Console.WriteLine("------------------------");
+            }
+
+            var courses = new List<Course>();
+
+            courses.Add(new Course("Fundamentos OOP", "fundamentos-oop"));
+            courses.Add(new Course("Fundamentos C#", "fundamentos-csharp"));
+            courses.Add(new Course("Fundamentos ASP.NET", "fundamentos-dotnet"));
+
+            var careers = new List<Career>();
+            var careerDotNet = new Career("Especialista .NET", "especialista-dotnet");
+            var carrerItem2 = new CareerItem(2, "Aprenda OOP", "", null);
+            var carrerItem = new CareerItem(1, "Comece por aqui", "", null);
+            var carrerItem3 = new CareerItem(3, "Aprenda .NET", "", null);
+            careerDotNet.Items.Add(carrerItem2);
+            careerDotNet.Items.Add(carrerItem);
+            careerDotNet.Items.Add(carrerItem3);
+
+            careers.Add(careerDotNet);
+
+            foreach (var career in careers)
+            {
+                Console.WriteLine(career.Title);
+
+                foreach (var item in career.Items.OrderBy(x => x.Order))
+                {
+                    Console.WriteLine($"{item.Order} - {item.Title}");
+                }
             }
         }
     }
