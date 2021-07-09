7b28e7dadc97c69035125640d19f0ec411d68b79
Daniel Cruz <d.afc98@gmail.com>
2021-07-09 18:58:09 -0300

    join

--- backenddotnet/fundamentos-sql-server/query4.sql
+++ backenddotnet/fundamentos-sql-server/query4.sql
@@ -1,4 +1,13 @@
-
 SELECT a.Id   AS Codigo,
        a.Nome AS Nome
   FROM Curso a
+
+
+--JOINS
+
+SELECT a.Id AS CursoId,
+       a.Nome AS Curso,
+       b.Id AS CategoriaId,
+       b.Nome AS Categoria
+  FROM Curso        a
+     JOIN Categoria b ON a.CategoriaId = b.Id
