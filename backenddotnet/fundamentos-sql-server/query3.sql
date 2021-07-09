32d094bbe11547e2640c1874db034c8be2296726
Daniel Cruz <d.afc98@gmail.com>
2021-07-08 23:17:49 -0300

    min, max, count, avg, sum

--- backenddotnet/fundamentos-sql-server/query3.sql
+++ backenddotnet/fundamentos-sql-server/query3.sql
@@ -14,3 +14,19 @@ BEGIN TRANSACTION
     FROM Categoria a
     WHERE a.Id = 3
 ROLLBACK
+
+
+SELECT MIN(a.Id) AS MinId
+  FROM Categoria a
+
+SELECT MAX(a.Id) AS MaxId
+  FROM Categoria a
+
+SELECT COUNT(*) AS Qtde
+  FROM Categoria a
+
+SELECT AVG(a.Id) AS Media
+  FROM Categoria a
+
+SELECT SUM(a.Id) AS Soma
+  FROM Categoria a
