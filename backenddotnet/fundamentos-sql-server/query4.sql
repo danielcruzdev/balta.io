2727e005ba088ba9042c014eb4d3e3d643859c07
Daniel Cruz <d.afc98@gmail.com>
2021-07-09 19:10:35 -0300

    having

--- backenddotnet/fundamentos-sql-server/query4.sql
+++ backenddotnet/fundamentos-sql-server/query4.sql
@@ -26,4 +26,11 @@ SELECT a.Id, a.Nome
 SELECT a.Nome, COUNT(b.CategoriaId) AS Qtde
   FROM Categoria a
     JOIN Curso   b ON a.Id = b.CategoriaId
-  GROUP BY b.CategoriaId, a.Nome
+GROUP BY b.CategoriaId, a.Nome
+
+--HAVING
+SELECT a.Nome, COUNT(b.CategoriaId) AS Qtde
+  FROM Categoria a
+    JOIN Curso   b ON a.Id = b.CategoriaId
+GROUP BY b.CategoriaId, a.Nome
+HAVING COUNT(b.CategoriaId) >= 2
