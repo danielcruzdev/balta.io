8aa6374af7752aeaf98772462d6bf5aec923b4a5
Daniel Cruz <d.afc98@gmail.com>
2021-07-11 18:31:15 -0300

    instânciando conexão com o banco

--- backenddotnet/data-access-dapper/DataAccess/DataAccess/Program.cs
+++ backenddotnet/data-access-dapper/DataAccess/DataAccess/Program.cs
@@ -9,7 +9,10 @@ namespace DataAccess
         {
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
 
-
+            using(var connection = new SqlConnection(connectionString))
+            {
+                Console.WriteLine("Conectado!");
+            }
 
             Console.WriteLine("Hello World!");
         }
