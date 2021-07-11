cad04b27bc53494f196cbb5e993a3b04da66517d
Daniel Cruz <d.afc98@gmail.com>
2021-07-11 18:23:37 -0300

    adicionando microsoft.data.sqlcliente no projeto

--- backenddotnet/data-access-dapper/DataAccess/DataAccess/Program.cs
+++ backenddotnet/data-access-dapper/DataAccess/DataAccess/Program.cs
@@ -7,6 +7,9 @@ namespace DataAccess
         static void Main(string[] args)
         {
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+
+            
+
             Console.WriteLine("Hello World!");
         }
     }
