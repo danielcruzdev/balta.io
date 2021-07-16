92a037dd0bd5d91d93dac6ba6b2c42849edeaa64
Daniel Cruz <d.afc98@gmail.com>
2021-07-15 22:41:22 -0300

    ajuste generic repository

--- backenddotnet/data-access-dapper/Blog/Blog/Repositories/Repository.cs
+++ backenddotnet/data-access-dapper/Blog/Blog/Repositories/Repository.cs
@@ -5,20 +5,20 @@ using System.Linq;
 
 namespace Blog.Repositories
 {
-    public class Repository<T> where T : class
+    public class Repository<TModel> where TModel : class
     {
         private readonly SqlConnection _connection;
 
         public Repository(SqlConnection connection) => _connection = connection;
 
-        public void Create(T model) => _connection.Insert(model);
+        public void Create(TModel model) => _connection.Insert(model);
 
-        public List<T> Read() => _connection.GetAll<T>().ToList();
+        public List<TModel> Read() => _connection.GetAll<TModel>().ToList();
 
-        public T Read(int id) => _connection.Get<T>(id);
+        public TModel Read(int id) => _connection.Get<TModel>(id);
 
-        public void Update(T model) => _connection.Update(model);
+        public void Update(TModel model) => _connection.Update(model);
 
-        public void Delete(T model) => _connection.Delete(model);
+        public void Delete(TModel model) => _connection.Delete(model);
     }
 }
