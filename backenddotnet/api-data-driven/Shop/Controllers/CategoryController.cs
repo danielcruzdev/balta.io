0bdb3c95d09a0823eacf9ab071331b1525f26b50
Daniel Cruz <d.afc98@gmail.com>
2021-07-18 23:23:47 -0300

    utilizando rotas e criando controller de categorias

---
+++ backenddotnet/api-data-driven/Shop/Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Controllers
+{
+    [Route("categories")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [Route("")]
+        public string MeuMetodo()
+        {
+            return "Olá mundo!";
+        }
+    }
+}
