7525d74cb370a922d0893dca07598e74bb8b73dc
Daniel Cruz <d.afc98@gmail.com>
2021-07-22 23:36:49 -0300

    ajuste startup

--- backenddotnet/api-data-driven/Shop/Startup.cs
+++ backenddotnet/api-data-driven/Shop/Startup.cs
@@ -53,9 +53,8 @@ namespace Shop
                 };
             });
 
-            //services.AddDbContext<DataContext>(x => x.UseInMemoryDatabase("Database"));
-            services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("connectionString")));
-            services.AddScoped<DataContext, DataContext>();
+            services.AddDbContext<DataContext>(x => x.UseInMemoryDatabase("Database"));
+            //services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("connectionString")));
 
             services.AddSwaggerGen(c =>
             {
