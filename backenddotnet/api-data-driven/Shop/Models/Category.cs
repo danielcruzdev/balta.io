567233e5b3c51c8ede24b9b3ce3098bfbc8ac542
Daniel Cruz <d.afc98@gmail.com>
2021-07-23 23:16:28 -0300

    ajuste model category

--- backenddotnet/api-data-driven/Shop/Models/Category.cs
+++ backenddotnet/api-data-driven/Shop/Models/Category.cs
@@ -11,5 +11,7 @@ namespace Shop.Models
         [MaxLength(60, ErrorMessage = "Este campo deve conter entre 3 e 60 catacteres")]
         [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 catacteres")]
         public string Title { get; set; }
+
+        public string Summary { get; set; }
     }
 }
