68a7ae509580cb01911108a7f69d199abe0b29e9
Daniel Cruz <d.afc98@gmail.com>
2021-07-25 19:43:39 -0300

    datacontext correto

--- backenddotnet/api-data-driven/Shop/Data/DataContext.cs
+++ backenddotnet/api-data-driven/Shop/Data/DataContext.cs
@@ -12,6 +12,5 @@ namespace Shop.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<User> Users { get; set; }
-        public DbSet<User> Users { get; set; }
     }
 }
