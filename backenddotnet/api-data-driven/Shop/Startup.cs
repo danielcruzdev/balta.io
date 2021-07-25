9e4de6481480640b376e4b68360c28ac2663aaa5
Daniel Cruz <d.afc98@gmail.com>
2021-07-24 22:58:49 -0300

    add cors

--- backenddotnet/api-data-driven/Shop/Startup.cs
+++ backenddotnet/api-data-driven/Shop/Startup.cs
@@ -26,7 +26,7 @@ namespace Shop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddCors();
+            services.AddCors();
             services.AddResponseCompression(opt =>
             {
                 opt.Providers.Add<GzipCompressionProvider>();
