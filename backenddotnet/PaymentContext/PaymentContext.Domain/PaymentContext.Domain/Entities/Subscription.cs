63253ec1ca92499f44fabda973141e0a136889bc
Daniel Cruz <d.afc98@gmail.com>
2021-07-27 23:04:22 -0300

    ajuste subscription entity

--- backenddotnet/PaymentContext/PaymentContext.Domain/PaymentContext.Domain/Entities/Subscription.cs
+++ backenddotnet/PaymentContext/PaymentContext.Domain/PaymentContext.Domain/Entities/Subscription.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PaymentContext.Domain.Entities
 {
     public class Subscription
     {
-        public Subscription()
+        private IList<Payment> _payments;
+
+        public Subscription(DateTime? expireDate)
         {
-            Payments = new List<Payment>();
+            CreateDate = DateTime.Now;
+            LastUpdateDate = DateTime.Now;
+            ExpireDate = expireDate;
+            Active = true;
+            _payments = new List<Payment>();
         }
 
         public DateTime CreateDate { get; set; }
         public DateTime LastUpdateDate { get; set; }
         public DateTime? ExpireDate { get; set; }
         public bool Active { get; set; }
-        public List<Payment> Payments { get; set; }
+        public IReadOnlyCollection<Payment> Payments { get => _payments.ToArray(); }
     }
 }
