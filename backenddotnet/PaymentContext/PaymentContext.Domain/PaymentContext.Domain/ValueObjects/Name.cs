095e5505aa2ce69dfb329117551efdbbdc5b9d44
Daniel Cruz <d.afc98@gmail.com>
2021-07-28 22:52:20 -0300

    utilizando flunt para validação

--- backenddotnet/PaymentContext/PaymentContext.Domain/PaymentContext.Domain/ValueObjects/Name.cs
+++ backenddotnet/PaymentContext/PaymentContext.Domain/PaymentContext.Domain/ValueObjects/Name.cs
@@ -1,4 +1,5 @@
-﻿using PaymentContext.Shared.ValueObjects;
+﻿using Flunt.Validations;
+using PaymentContext.Shared.ValueObjects;
 
 namespace PaymentContext.Domain.ValueObjects
 {
@@ -9,10 +10,12 @@ namespace PaymentContext.Domain.ValueObjects
             FirstName = firstName;
             LastName = lastName;
 
-
-
-            if (string.IsNullOrEmpty(FirstName))
-                AddNotification("Name.FirstName", "Nome inválido!");
+            AddNotifications(new Contract()
+                    .Requires()
+                    .HasMinLen(FirstName, 3, "Name.FirstName", "Nome deve conter pelo menos 3 caracteres")
+                    .HasMinLen(LastName, 3, "Name.LastName", "Sobrenome deve conter pelo menos 3 caracteres")
+                    .HasMaxLen(FirstName, 40, "Name.FirstName", "Nome deve conter no máximo 40 caracteres")
+                    .HasMaxLen(LastName, 40, "Name.LastName", "Sobrenome deve conter no máximo 40 caracteres"));
         }
 
         public string FirstName { get; private set; }
