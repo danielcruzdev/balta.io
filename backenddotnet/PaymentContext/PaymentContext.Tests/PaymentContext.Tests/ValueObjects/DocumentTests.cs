44da8b53595719e14b6942abed06ff2e6c92ad5f
Daniel Cruz <d.afc98@gmail.com>
2021-07-29 22:55:47 -0300

    criando testes

---
+++ backenddotnet/PaymentContext/PaymentContext.Tests/PaymentContext.Tests/ValueObjects/DocumentTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests.ValueObjects
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        //Red, Green, Refactor
+        [TestMethod]
+        public void ShouldReturnErrorWhenCNPJIsInvalid()
+        {
+            var doc = new Document("123", Domain.Enums.EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Invalid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnSuccessWhenCNPJIsValid()
+        {
+            var doc = new Document("08272712000171", Domain.Enums.EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Valid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenCPFIsInvalid()
+        {
+            var doc = new Document("123", Domain.Enums.EDocumentType.CPF);
+            Assert.IsTrue(doc.Invalid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnSuccessWhenCPFIsValid()
+        {
+            var doc = new Document("41356439810", Domain.Enums.EDocumentType.CPF);
+            Assert.IsTrue(doc.Valid);
+        }
+    }
+}
