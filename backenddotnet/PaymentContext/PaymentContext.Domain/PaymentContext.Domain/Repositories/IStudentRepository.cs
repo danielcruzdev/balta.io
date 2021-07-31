a09db9d12d8696243e07f8112505fd52c9946a0f
Daniel Cruz <d.afc98@gmail.com>
2021-07-30 23:27:26 -0300

    ajuste repository interface

--- backenddotnet/PaymentContext/PaymentContext.Domain/PaymentContext.Domain/Repositories/IStudentRepository.cs
+++ backenddotnet/PaymentContext/PaymentContext.Domain/PaymentContext.Domain/Repositories/IStudentRepository.cs
@@ -1,7 +1,11 @@
-﻿namespace PaymentContext.Domain.Repositories
+﻿using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Domain.Repositories
 {
     public interface IStudentRepository
     {
-
+        bool DocumentExists(string document);
+        bool EmailExists(string email);
+        void CreateSubscription(Student student);
     }
 }
