071e88d73e3d77967cf89e4ab8fde4b042a48054
Daniel Cruz <d.afc98@gmail.com>
2021-08-01 19:57:40 -0300

    utilizando readOnlyCollection

---
+++ backenddotnet/Store/Store.Domain/Store.Domain/Entities/Order.cs
@@ -0,0 +1,48 @@
+﻿using Store.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Store.Domain.Entities
+{
+    public class Order : Entity
+    {
+        public Order(Customer customer, decimal deliveryFee, Discount discount)
+        {
+            Customer = customer;
+            Date = DateTime.Now;
+            Number = Guid.NewGuid().ToString().Substring(0, 8);
+            _items = new List<OrderItem>();
+            DeliveryFee = deliveryFee;
+            Discount = discount;
+            Status = EOrderStatus.WaitingPayment;
+        }
+
+        private IList<OrderItem> _items;
+        public Customer Customer { get; private set; }
+        public DateTime Date { get; private set; }
+        public string Number { get; private set; }
+        public IReadOnlyCollection<OrderItem> Items { get => _items.ToArray(); }
+        public decimal DeliveryFee { get; private set; }
+        public Discount Discount { get; private set; }
+        public EOrderStatus Status { get; private set; }
+
+        public void AddItem(Product product, int quantity)
+        {
+            var item = new OrderItem(product, quantity);
+            _items.Add(item);
+        }
+
+        public decimal Total()
+        {
+            decimal total = 0;
+            foreach (var item in Items)
+                total += item.Total();
+
+            total += DeliveryFee;
+            total -= Discount != null ? Discount.Value() : 0;
+
+            return total;
+        }
+    }
+}
