16f4506177d69f55ca1c6a12334c8aceb6f1e65d
Daniel Cruz <d.afc98@gmail.com>
2021-07-31 21:04:42 -0300

    create handler subscriptions

--- backenddotnet/PaymentContext/PaymentContext.Domain/PaymentContext.Domain/Handlers/SubscriptionHandler.cs
+++ backenddotnet/PaymentContext/PaymentContext.Domain/PaymentContext.Domain/Handlers/SubscriptionHandler.cs
@@ -14,7 +14,8 @@ namespace PaymentContext.Domain.Handlers
     public class SubscriptionHandler :
             Notifiable,
             IHandler<CreateBoletoSubscriptionCommand>,
-            IHandler<CreatePayPalSubscriptionCommand>
+            IHandler<CreatePayPalSubscriptionCommand>,
+            IHandler<CreateCreditCardSubscriptionCommand>
     {
         private readonly IStudentRepository _repository;
         private readonly IEmailService _emailService;
@@ -109,6 +110,56 @@ namespace PaymentContext.Domain.Handlers
             var payment = new PayPalPayment(
                 email,
                 command.TransactionCode,
+                command.PaidDate,
+                command.ExpireDate,
+                command.Total,
+                command.TotalPaid,
+                command.Payer,
+                new Document(command.PayerDocument, command.PayerDocumentType),
+                address
+            );
+
+            // Relacionamentos
+            subscription.AddPayment(payment);
+            student.AddSubscription(subscription);
+
+            // Agrupar as Validações
+            AddNotifications(name, document, email, address, student, subscription, payment);
+
+            // Salvar as Informações
+            _repository.CreateSubscription(student);
+
+            // Enviar E-mail de boas vindas
+            _emailService.Send(student.Name.ToString(), student.Email.Address, "bem vindo ao balta.io", "Sua assinatura foi criada");
+
+            // Retornar informações
+            return new CommandResult(true, "Assinatura realizada com sucesso");
+        }
+
+        public ICommandResult Handle(CreateCreditCardSubscriptionCommand command)
+        {
+            // Verificar se Documento já está cadastrado
+            if (_repository.DocumentExists(command.Document))
+                AddNotification("Document", "Este CPF já está em uso");
+
+            // Verificar se E-mail já está cadastrado
+            if (_repository.EmailExists(command.Email))
+                AddNotification("Email", "Este E-mail já está em uso");
+
+            // Gerar os VOs
+            var name = new Name(command.FirstName, command.LastName);
+            var document = new Document(command.Document, EDocumentType.CPF);
+            var email = new Email(command.Email);
+            var address = new Address(command.Street, command.Number, command.Neighborhood, command.City, command.State, command.Country, command.ZipCode);
+
+            // Gerar as Entidades
+            var student = new Student(name, document, email);
+            var subscription = new Subscription(DateTime.Now.AddMonths(1));
+            // Só muda a implementação do Pagamento
+            var payment = new CreditCardPaymento(
+                command.CardHolderName,
+                command.CardNumber,
+                command.LastTransactionNumber,
                 command.PaidDate,
                 command.ExpireDate,
                 command.Total,
