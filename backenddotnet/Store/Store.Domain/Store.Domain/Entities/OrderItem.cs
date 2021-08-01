87b6e4b289845ee1d66cb1e26f7b8e22be717131
Daniel Cruz <d.afc98@gmail.com>
2021-08-01 19:39:55 -0300

    create ordem item entity

---
+++ backenddotnet/Store/Store.Domain/Store.Domain/Entities/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿namespace Store.Domain.Entities
+{
+    public class OrderItem : Entity
+    {
+        public OrderItem(Product product, decimal price, int quantity)
+        {
+            Product = product;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public Product Product { get; private set; }
+        public decimal Price { get; private set; }
+        public int Quantity { get; private set; }
+
+        public decimal Total() => Price * Quantity;
+    }
+}
