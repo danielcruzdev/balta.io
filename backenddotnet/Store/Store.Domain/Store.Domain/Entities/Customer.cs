7fb805aa8f3b7baa054ddd2355253550c477dbc3
Daniel Cruz <d.afc98@gmail.com>
2021-08-01 18:27:53 -0300

    create customer entity

---
+++ backenddotnet/Store/Store.Domain/Store.Domain/Entities/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace Store.Domain.Entities
+{
+    public class Customer
+    {
+        public Customer(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+}
