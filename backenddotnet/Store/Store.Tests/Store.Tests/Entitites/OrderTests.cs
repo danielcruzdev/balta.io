b4061260bce524eaf15bc93ef70a436b19c15e15
Daniel Cruz <d.afc98@gmail.com>
2021-08-02 21:11:45 -0300

    criando primeiro teste do pedido

---
+++ backenddotnet/Store/Store.Tests/Store.Tests/Entitites/OrderTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store.Domain.Entities;
+using System;
+
+namespace Store.Tests.Entitites
+{
+    [TestClass]
+    public class OrderTests
+    {
+        private readonly Customer _customer = new Customer("Daniel Cruz", "d.afc98@gmail.com");
+        private readonly Product _product = new Product("Produto 1", 10, true);
+        private readonly Discount _discount = new Discount(10, DateTime.Now.AddDays(5));
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void DadoUmNovoPedidoValidoEleDeveGerarUmNumeroCom8Caracteres()
+        {
+            var order = new Order(_customer, 0, null);
+            Assert.AreEqual(8, order.Number.Length);
+        }
+    }
+}
