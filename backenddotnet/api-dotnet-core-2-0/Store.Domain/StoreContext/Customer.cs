6e73b29c8d86000531cfe15d3de1bcf55fd01db2
Daniel Cruz <d.afc98@gmail.com>
2021-08-30 22:58:02 -0300

    create customer context

---
+++ backenddotnet/api-dotnet-core-2-0/Store.Domain/StoreContext/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Store.Domain.StoreContext
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public decimal Salary { get; set; }
+
+        public void Register() { }
+
+        public void OnRegister() { }
+    }
+}
