684a60e948efb55726c1c3c545dc54e85d071d35
Daniel Cruz <d.afc98@gmail.com>
2021-08-30 23:17:01 -0300

    OrderItem entity

---
+++ backenddotnet/api-dotnet-core-2-0/Store.Domain/StoreContext/OrderItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Store.Domain.StoreContext
+{
+    public class OrderItem
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+    }
+}
