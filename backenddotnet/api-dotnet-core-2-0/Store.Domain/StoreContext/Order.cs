5b07c4a83b923afe42fb69a1d67f527274d57f4c
Daniel Cruz <d.afc98@gmail.com>
2021-08-30 23:16:46 -0300

    order entity

---
+++ backenddotnet/api-dotnet-core-2-0/Store.Domain/StoreContext/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Store.Domain.StoreContext
+{
+    public class Order
+    {
+        public Order()
+        {
+            Items = new List<OrderItem>();
+        }
+
+        public Customer Customer { get; set; }
+        public string Number { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string Status { get; set; }
+        public IList<OrderItem> Items { get; set; }
+        public IList<Delivery> Deliveries { get; set; }
+
+        public void Place() { }
+    }
+}
