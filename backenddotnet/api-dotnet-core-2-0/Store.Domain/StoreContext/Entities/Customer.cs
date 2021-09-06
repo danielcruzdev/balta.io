4e98d4a41c029506d7b8db32db0e9d404d021615
Daniel Cruz <d.afc98@gmail.com>
2021-09-06 12:04:27 -0300

    create constructor

--- backenddotnet/api-dotnet-core-2-0/Store.Domain/StoreContext/Entities/Customer.cs
+++ backenddotnet/api-dotnet-core-2-0/Store.Domain/StoreContext/Entities/Customer.cs
@@ -2,6 +2,16 @@
 {
     public class Customer
     {
+        public Customer(string firstName, string lastName, string document, string email, string phone, string address)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Document = document;
+            Email = email;
+            Phone = phone;
+            Address = address;
+        }
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Document { get; set; }
