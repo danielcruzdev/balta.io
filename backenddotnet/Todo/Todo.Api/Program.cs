6d25e96764d6d667770f0f20c577a6537beb2383
Daniel Cruz <d.afc98@gmail.com>
2021-09-29 22:27:00 -0300

    todo item

--- backenddotnet/Todo/Todo.Api/Program.cs
+++ backenddotnet/Todo/Todo.Api/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace Todo.Api
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
