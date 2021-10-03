918332a4f5ac89e6fc0e638ccf85bcb972f37605
Daniel Cruz <d.afc98@gmail.com>
2021-10-03 20:25:08 -0300

    update command todo

---
+++ backenddotnet/Todo/Todo.Domain/Commands/UpdateTodoCommands.cs
@@ -0,0 +1,36 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using System;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands
+{
+    public class UpdateTodoCommands : Notifiable, ICommand
+    {
+        public UpdateTodoCommands()
+        {
+
+        }
+
+        public UpdateTodoCommands(Guid id, string title, string user)
+        {
+            Id = id;
+            Title = title;
+            User = user;
+        }
+
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string User { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(
+                new Contract()
+                .Requires()
+                .HasMinLen(Title, 3, "Title", "Por favor, descreva melhor esta tarefa!")
+                .HasMinLen(User, 6, "User", "Usuário inválido!")
+            );
+        }
+    }
+}
