1e3f6ccb69c232dec36e8ca69064eca1e654cffd
Daniel Cruz <d.afc98@gmail.com>
2021-10-03 21:45:43 -0300

    repository no handler

--- backenddotnet/Todo/Todo.Domain/Handlers/TodoHandler.cs
+++ backenddotnet/Todo/Todo.Domain/Handlers/TodoHandler.cs
@@ -2,11 +2,16 @@
 using Todo.Domain.Commands;
 using Todo.Domain.Commands.Contracts;
 using Todo.Domain.Handlers.Contracts;
+using Todo.Domain.Repositories;
 
 namespace Todo.Domain.Handlers
 {
     public class TodoHandler : Notifiable, IHandler<CreateTodoCommand>
     {
+        private readonly ITodoRepository _todoRepository;
+
+        public TodoHandler(ITodoRepository todoRepository) => _todoRepository = todoRepository;
+
         public ICommandResult Handle(CreateTodoCommand command)
         {
             throw new System.NotImplementedException();
