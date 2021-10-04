d5fa98a5251fd54a68bda6bc7c0b64d1ee388aa2
Daniel Cruz <d.afc98@gmail.com>
2021-10-03 21:44:05 -0300

    crate todo handler

---
+++ backenddotnet/Todo/Todo.Domain/Handlers/TodoHandler.cs
@@ -0,0 +1,15 @@
+﻿using Flunt.Notifications;
+using Todo.Domain.Commands;
+using Todo.Domain.Commands.Contracts;
+using Todo.Domain.Handlers.Contracts;
+
+namespace Todo.Domain.Handlers
+{
+    public class TodoHandler : Notifiable, IHandler<CreateTodoCommand>
+    {
+        public ICommandResult Handle(CreateTodoCommand command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
