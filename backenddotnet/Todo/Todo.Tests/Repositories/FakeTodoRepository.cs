79dab00b329aed36f566e5ca2820a601f165b253
Daniel Cruz <d.afc98@gmail.com>
2021-10-07 22:57:06 -0300

    todo repository

--- backenddotnet/Todo/Todo.Tests/Repositories/FakeTodoRepository.cs
+++ backenddotnet/Todo/Todo.Tests/Repositories/FakeTodoRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Todo.Domain.Entities;
 using Todo.Domain.Repositories;
 
@@ -11,9 +12,29 @@ namespace Todo.Tests.Repositories
 
         }
 
+        public IEnumerable<TodoItem> GetAll(string user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TodoItem> GetAllDone(string user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TodoItem> GetAllUndone(string user)
+        {
+            throw new NotImplementedException();
+        }
+
         public TodoItem GetById(Guid id, string user)
         {
             return new TodoItem("Titulo Novo!", DateTime.Now, "Daniel Cruz");
+        }
+
+        public IEnumerable<TodoItem> GetByPeriod(string user, DateTime date, bool done)
+        {
+            throw new NotImplementedException();
         }
 
         public void Update(TodoItem todo)
