c00b0837b2d1d19ec325f5b60d3ccf990f9c6782
Daniel Cruz <d.afc98@gmail.com>
2021-10-07 22:56:48 -0300

    startup

--- backenddotnet/Todo/Todo.Api/Startup.cs
+++ backenddotnet/Todo/Todo.Api/Startup.cs
@@ -1,21 +1,42 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Todo.Domain.Handlers;
+using Todo.Domain.Repositories;
+using Todo.Infra.Contexts;
+using Todo.Infra.Repositories;
 
 namespace Todo.Api
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
 
+            services.AddTransient<ITodoRepository, TodoRepository>();
+            services.AddTransient<TodoHandler, TodoHandler>();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
