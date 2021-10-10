82448a288d6fdf83d3971261b450f0b1b9fd7125
Daniel Cruz <d.afc98@gmail.com>
2021-10-10 17:48:16 -0300

    startup and controller todo

---
+++ backenddotnet/Todo/Todo.Api/Controllers/TodoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Todo.Domain.Commands;
+using Todo.Domain.Handlers;
+
+namespace Todo.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/todos")]
+    public class TodoController : ControllerBase
+    {
+
+        [HttpPost]
+        public GenericCommandResult Create([FromBody] CreateTodoCommand command,
+                                           [FromServices] TodoHandler handler)
+        {
+            command.User = "danielcruz";
+            return (GenericCommandResult)handler.Handle(command);
+        }
+    }
+}
