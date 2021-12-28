341b4a8e272dd60cc56f351aa2648f48b156e48f
Daniel Cruz <d.afc98@gmail.com>
2021-12-28 17:55:21 -0300

    crete db

---
+++ fundamentos-dotnet-6/Todo/Data/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.Models;
+
+namespace Todo.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<TodoModel> Todo { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite("DataSource=app.db;Cache=Shared");
+    }
+}
