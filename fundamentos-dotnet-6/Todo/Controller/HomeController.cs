30dd642c5805eebfcf4f1fe2a85f2c6d25059ce0
Daniel Cruz <d.afc98@gmail.com>
2021-12-29 13:37:52 -0300

    controller home

---
+++ fundamentos-dotnet-6/Todo/Controller/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Todo.Controller
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("/")]
+        public ActionResult<string> Get()
+        {
+            return Ok("Hello World!");
+        } 
+    }
+}
