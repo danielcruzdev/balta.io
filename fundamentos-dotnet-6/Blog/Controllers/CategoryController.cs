8107945826c19d1c0792cb6ce4223165830ef68c
Daniel Cruz <d.afc98@gmail.com>
2021-12-30 19:53:21 -0300

    create extension and result view padronizado

--- fundamentos-dotnet-6/Blog/Controllers/CategoryController.cs
+++ fundamentos-dotnet-6/Blog/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using Blog.Data;
+using Blog.Extensions;
 using Blog.Models;
+using Blog.ViewModels.Categories;
+using Blog.ViewModels.Shared;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,11 +17,11 @@ namespace Blog.Controllers
             try
             {
                 var categories = await context.Categories.ToListAsync();
-                return Ok(categories);
+                return Ok(new ResultViewModel<List<Category>>(categories));
             }
-            catch (Exception)
+            catch
             {
-                return StatusCode(500, "Falha interna no servidor!");
+                return StatusCode(500, new ResultViewModel<List<Category>>("Falha interna no servidor!"));
             }
         }
 
@@ -31,48 +34,61 @@ namespace Blog.Controllers
                 var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
 
                 if (category == null)
-                    return NotFound();
+                    return NotFound(new ResultViewModel<Category>("Categoria não encontrada!"));
 
-                return Ok(category);
+                return Ok(new ResultViewModel<Category>(category));
             }
-            catch (Exception)
+            catch
             {
-                return StatusCode(500, "Falha interna no servidor!");
+                return StatusCode(500, new ResultViewModel<Category>("Falha interna no servidor!"));
             }
         }
 
         [HttpPost("v1/categories")]
-        public async Task<IActionResult> PostAsync([FromBody] Category model,
+        public async Task<IActionResult> PostAsync([FromBody] EditorCategoryViewModel model,
                                                    [FromServices] BlogDataContext context)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(new ResultViewModel<Category>(ModelState.GetErrors()));
+
             try
             {
-                await context.Categories.AddAsync(model);
+                var category = new Category
+                {
+                    Id = 0,
+                    Name = model.Name,
+                    Slug = model.Slug.ToLower(),
+                };
+
+                await context.Categories.AddAsync(category);
                 await context.SaveChangesAsync();
 
-                return Created($"v1/categories/{model.Id}", model);
+                return Created($"v1/categories/{category.Id}", new ResultViewModel<Category>(category));
             }
             catch (DbUpdateException)
             {
-                return StatusCode(500, "Não foi possível incluir a categoria!");
+                return StatusCode(500, new ResultViewModel<Category>("Não foi possível incluir a categoria!"));
             }
             catch (Exception)
             {
-                return StatusCode(500, "Falha interna no servidor!");
+                return StatusCode(500, new ResultViewModel<Category>("Falha interna no servidor!"));
             }
         }
 
         [HttpPut("v1/categories/{id:int}")]
         public async Task<IActionResult> PutAsync([FromRoute] int id,
-                                                   [FromBody] Category model,
+                                                   [FromBody] EditorCategoryViewModel model,
                                                    [FromServices] BlogDataContext context)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(new ResultViewModel<Category>(ModelState.GetErrors()));
+
             try
             {
                 var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
 
                 if (category == null)
-                    return NotFound();
+                    return NotFound(new ResultViewModel<Category>("Categoria não encontrada!"));
 
                 category.Name = model.Name;
                 category.Slug = model.Slug;
@@ -80,15 +96,15 @@ namespace Blog.Controllers
                 context.Update(category);
                 await context.SaveChangesAsync();
 
-                return Ok(category);
+                return Ok(new ResultViewModel<Category>(category));
             }
             catch (DbUpdateException)
             {
-                return StatusCode(500, "Não foi possível atualizar a categoria!");
+                return StatusCode(500, new ResultViewModel<Category>("Não foi possível atualizar a categoria!"));
             }
             catch (Exception)
             {
-                return StatusCode(500, "Falha interna no servidor!");
+                return StatusCode(500, new ResultViewModel<Category>("Falha interna no servidor!"));
             }
         }
 
@@ -101,20 +117,20 @@ namespace Blog.Controllers
                 var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
 
                 if (category == null)
-                    return NotFound();
+                    return NotFound(new ResultViewModel<Category>("Categoria não encontrada!"));
 
                 context.Remove(category);
                 await context.SaveChangesAsync();
 
-                return Ok(category);
+                return Ok(new ResultViewModel<Category>(category));
             }
             catch (DbUpdateException)
             {
-                return StatusCode(500, "Não foi possível excluir a categoria!");
+                return StatusCode(500, new ResultViewModel<Category>("Não foi possível excluir a categoria!"));
             }
             catch (Exception)
             {
-                return StatusCode(500, "Falha interna no servidor!");
+                return StatusCode(500, new ResultViewModel<Category>("Falha interna no servidor!"));
             }
         }
     }
---
+++ fundamentos-dotnet-6/Blog/ViewModels/Shared/ResultViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Blog.ViewModels.Shared
+{
+    public class ResultViewModel<T>
+    {
+
+        public ResultViewModel(T data, List<string> erros)
+        {
+            Data = data;
+            Erros = erros;
+        }
+        public ResultViewModel(T data)
+        {
+            Data = data;
+        }
+
+        public ResultViewModel(List<string> erros)
+        {
+            Erros = erros;
+        }
+
+        public ResultViewModel(string error)
+        {
+            Erros.Add(error);
+        }
+
+        public T Data { get; private set; }
+        public List<string> Erros { get; private set; } = new();
+    }
+}
