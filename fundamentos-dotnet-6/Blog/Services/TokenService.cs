cb6ead079cd17f30b2af0ee225d04fdb7dd0024a
Daniel Cruz <d.afc98@gmail.com>
2022-01-01 22:31:59 -0300

    generate token

---
+++ fundamentos-dotnet-6/Blog/Services/TokenService.cs
@@ -0,0 +1,29 @@
+﻿using Blog.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Blog.Services
+{
+    public class TokenService
+    {
+        public string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new (ClaimTypes.Name, user.Name),
+                    new (ClaimTypes.Role, user.Roles[1].Name)
+                })
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
