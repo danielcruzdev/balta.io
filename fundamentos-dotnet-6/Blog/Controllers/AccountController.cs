79767d41c744015ee33ea2db708f794eb9559ee8
Daniel Cruz <d.afc98@gmail.com>
2022-01-01 22:32:08 -0300

    account controller

---
+++ fundamentos-dotnet-6/Blog/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+        [HttpPost("v1/login")]
+        public IActionResult Login([FromServices] TokenService tokenService)
+        {
+            return Ok(tokenService.GenerateToken(null));
+        }
+    }
+}
