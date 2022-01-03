9ba7bfbda84a1a29e508582019ff97443bf0b8f7
Daniel Cruz <d.afc98@gmail.com>
2022-01-02 23:39:01 -0300

    using api key

--- fundamentos-dotnet-6/Blog/Configuration.cs
+++ fundamentos-dotnet-6/Blog/Configuration.cs
@@ -2,6 +2,8 @@
 {
     public static class Configuration
     {
-        public static string JwtKey { get; set; } = "evZbbuXl80iODM3iZnw8bw";
+        public static string JwtKey = "evZbbuXl80iODM3iZnw8bw";
+        public static string ApiKeyName = "api_key";
+        public static string ApiKey = "curso_api_G2P9C7nH1UWdtE19Kgz69Q==";
     }
 }
